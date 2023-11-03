6bc4f0917d1fcea516c5a52bee94a932c7fc8561
Jeffrey <jefbfitzp33@outlook.com>
2023-11-03 12:40:20 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+
+function writeCards (Array, event){
+    let blankArray = []
+    for(let i=0; i < Array.length; i++ ){
+        blankArray.push(`Thank you, ${Array[i]}, for the wonderful ${event} gift!`)
+    }
+    return blankArray;
+}
+
+
+
+
+function countDown(number){
+    let start = number;
+    while (start >= 0) {
+        console.log(start--);
+    }
+
+}
